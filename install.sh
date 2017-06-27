be3d1b1eb05c05ddd55d4cd8ad623de268ab9656
SILexRaze <victor.fonne@live.fr>
2017-06-27 15:14:51 +0200

    add few lines for oh-my-zsh and candy theme

--- install.sh
+++ install.sh
@@ -2,3 +2,5 @@
 mkdir -p ~/.vim/colors
 wget https://raw.githubusercontent.com/gkjgh/cobalt/master/colors/cobalt.vim -O ~/.vim/colors/cobalt.vim
 cat vimrc > ~/.vimrc
+sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+cat ~/.zshrc | grep "ZSH_THEME" | sed "s/robbyrussell/candy/" >> ~/.zshrc
